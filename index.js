98eaec1b4adeee1de090256c0ad2914886516005
thisiselysa <herdianielysa@gmail.com>
2026-02-24 11:35:55 +0700

    update server

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const app = express();
 app.get('/', (req, res) => {
     res.send('HELLO ELLLLLLLL');
 });
+app.get('/say/:greeting', (req, res) => {
+    const greeting = req.params.greeting;
+    res.send(greeting);
+});
 
 app.listen(5000, () => {
     console.log('Server running on http://localhost:5000');
